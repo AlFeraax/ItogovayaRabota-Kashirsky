e892f821520a798615f8f72f2f2723a2273572bf
AlFeraax <grigolin@mail.ru>
2023-01-14 15:58:19 +0300

    Добавил код программы

---
+++ Program/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintArray(string[] arr)
+{
+    Console.Write("[" + arr[0] + " ");
+    for (int i = 1; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i]);
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите колличество элементов, которые вы собираетесь ввести далее: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+
+string[] array = new string[size];
+int position = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-й элемент");
+    string element = Console.ReadLine() ?? "0";
+    if (element.Length <= 3)
+    {
+        array[position] = element;
+        position++;
+    }
+}
+Console.WriteLine();
+PrintArray(array);
